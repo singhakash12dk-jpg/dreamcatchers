339b3f1ab7edd00ee87015b02f303e5de83b7b28
singhakash12dk <singhakash12dk@gmail.com>
2026-07-09 16:52:07 +0530

    Update GitHub Pages base for renamed repo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DreamCatchers_project/',
+  base: '/dreamcatchers/',
   build: {
     outDir: 'dist',
     sourcemap: false,
